bd7df8e45dbd648f777fadaec73030110f0502ca
bats <chris.bates@seer.net.au>
2014-02-26 13:51:08 +1100

    Added setup.py and license

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='store',
+    version='0.1',
+    author='C Bates',
+    author_email='chrsbats@gmail.com',
+    packages=['store'],
+    scripts=[],
+    url='https://github.com/chrsbats/store',
+    license='LICENSE.TXT',
+    description='Key value store with Filesystem and S3 backends',
+    long_description='Key value store with Filesystem and S3 backends',
+    install_requires=[
+        "boto==2.15.0",
+    ],
+)
